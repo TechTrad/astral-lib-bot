74cef59c729a2900de4a2f5a110599410d18d416
TechTrad <techtrad.lab@gmail.com>
2020-04-29 20:20:55 +0800

    disc bot

---
+++ bot.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+client.login(process.env.NzAzMTMyNTM1MzY2MDkwNzYy.XqlvTQ.N-NoznpYwGjlYksHl3vv1g5or3g);
